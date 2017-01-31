f41f100738b0d50d60664d1cc27de1c52ad90003
SAMUEL VERNECK <osamucaverneck@gmail.com>
2017-01-30 22:09:52 -0200

    src

---
+++ src/pages/home/home.ts
@@ -0,0 +1,94 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Geolocation } from 'ionic-native';
+
+/* @Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+    
+  }
+
+}
+
+import { Geolocation } from 'ionic-native';
+
+
+Geolocation.getCurrentPosition().then((resp) => {
+ // resp.coords.latitude
+ // resp.coords.longitude
+}).catch((error) => {
+  console.log('Error getting location', error);
+});
+
+let watch = Geolocation.watchPosition();
+watch.subscribe((data) => {
+ // data can be a set of coordinates, or an error (if an error occurred).
+ // data.coords.latitude
+ // data.coords.longitude
+}); */
+
+// Comecei do zero a partir daqui!
+
+
+declare var google;
+ 
+@Component({
+  selector: 'home-page',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+ 
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+ 
+  constructor(public navCtrl: NavController) {
+ 
+  }
+ 
+  ionViewLoaded(){
+    this.loadMap();
+  }
+ 
+  loadMap(){
+ 
+    let latLng = new google.maps.LatLng(-34.9290, 138.6010);
+ 
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+ 
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+ 
+  } , (err) => {
+      console.log(err);
+    });
+
+}
+
+/* function(success){
+    console.log(success);
+} */
+
+function(success) => {
+    console.log(success);
+}
+
+addMarker(){
+ 
+  let marker = new google.maps.Marker({
+    map: this.map,
+    animation: google.maps.Animation.DROP,
+    position: this.map.getCenter()
+  });
+ 
+  let content = "<h4>Information!</h4>";          
+ 
+  this.addInfoWindow(marker, content);
+ 
+}
